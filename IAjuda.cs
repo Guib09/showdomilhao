63a7c1d135aacdb56004988dff6648977c3a667e
showdomilhao <gui09bg@gmail.com>
2024-10-02 11:36:49 -0300

    show do milho p2

--- IAjuda.cs
+++ IAjuda.cs
@@ -1,7 +1,28 @@
 
 namespace Interfaces;
 
-public interface IAjudas
+public interface IAjuda
 {
+  public abstract class IAjuda
+  {
+    protected Button btnResp01;
+    protected Button btnResp02;
+    protected Button btnResp03;
+    protected Button btnResp05;
+    protected Frame FrameAjuda;
+       public void ConfiguraDesenho (Button btnResp01, ButtonResp02,
+       Button btnResp03,Button btnResp 04, Button btnResp05)
+       { 
+        this.btnResp01= btnResp01;
+        this.btnResp02= btnResp02;
+        this.btnResp03= btnResp03;
+        this.btnResp04= btnResp04;
+        this.btnResp05= btnResp05;
+       }
+       public void ConfiguraDesenho (Frame frameAjuda)
+       {
+          this.FrameAjuda= FrameAjuda;
+          public abstract void RealizaAjuda(Questao questao)
+       }
   void RealizarAjuda();
 }
---
+++ showdomilhao/Platforms/Android/Retirada.cs
@@ -0,0 +1,23 @@
+using showdomilhao;
+
+public class RetiradaErradas :IAjuda
+{
+    public override void RealizaAjuda (Questao questao);
+    
+        switch (Questao RespostaCorreta)
+        {
+            case 1:
+                  btnResp02.IsVisible=false;
+                  btnResp03.Isvisible=false;
+                  btnResp04.IsVisible=false;
+                  break;
+            case 2 : 
+                  btnResp01.IsVisible=false;
+                  btnResp03.IsVisible=false;
+                  btnResp05.IsVisible=false;
+                  break;
+
+                ]
+            ]
+                    
+    
